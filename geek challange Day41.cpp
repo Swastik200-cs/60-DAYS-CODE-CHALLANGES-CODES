d50df43a28399cbb54d2b364b09b14e0067d2beb
Swastik Chaudhuri <swastik.c2020@gmail.com>
2026-03-26 10:41:44 +0530

    Add files via upload

---
+++ geek challange Day41.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countPaths(int V, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto &e : edges){
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
+        }
+        priority_queue<pair<long long,int>, 
+                       vector<pair<long long,int>>, 
+                       greater<pair<long long,int>>> pq;
+        vector<long long> dist(V, LLONG_MAX);
+        vector<int> ways(V, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+        while(!pq.empty()){
+            auto [d, u] = pq.top();
+            pq.pop();       
+            for(auto &it : adj[u]){
+                int v = it.first;
+                int wt = it.second;
+                long long newDist = d + wt;
+                if(newDist < dist[v]){
+                    dist[v] = newDist;
+                    ways[v] = ways[u];
+                    pq.push({newDist, v});
+                }
+                else if(newDist == dist[v]){
+                    ways[v] += ways[u];
+                }
+            }
+        }
+        return ways[V-1];
+    }
+};
